5532493ea6504404e3513be6beb64d0b5db0cf14
ZilverQueen <muhammad.amier2002@gmail.com>
2022-09-21 17:44:28 +0000

    initial for lavender
    Signed-off-by: ZilverQueen <muhammad.amier2002@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -10,8 +10,7 @@ do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=ugg
-device.name2=ugglite
+device.name1=lavender
 supported.versions=
 supported.patchlevels=
 '; } # end properties
